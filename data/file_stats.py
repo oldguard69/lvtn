5e40481bc747fe72495734bd8d8a911536b30457
oldguard149 <tvthuan1409@gmail.com>
2021-08-24 09:22:52 +0700

    Add stats file

---
+++ data/file_stats.py
@@ -0,0 +1,17 @@
+import os
+LINE_BREAK = "=" * 50
+
+train_path = './train-1500'
+test_path = './test-500'
+
+def file_stats(path):
+    total = 0
+    for folder in os.listdir(path):
+        num_of_file = len(os.listdir(os.path.join(path, folder)))
+        total += num_of_file
+        print(f'{folder}: {num_of_file} files')
+    print(f'Total file in {path}: {total}')
+
+file_stats(train_path)
+print(LINE_BREAK)
+file_stats(test_path)
